b14d02c8f3f4e7ec864af108b297aa8e6f064f31
lmanohara <lmanohara99@gmail.com>
2023-05-29 12:53:31 +0100

    Insert records into OpenSearch

--- kakfka-consumer-opensearch/src/main/java/com/shperev/kafka/opensearch/OpenSearchConsumer.java
+++ kakfka-consumer-opensearch/src/main/java/com/shperev/kafka/opensearch/OpenSearchConsumer.java
@@ -1,13 +1,21 @@
 package com.shperev.kafka.opensearch;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
 
 public class OpenSearchConsumer {
 
@@ -16,15 +24,38 @@ public class OpenSearchConsumer {
     public static void main(String[] args) throws IOException {
 
         final String wikimediaIndex = "wikimedia";
+        final String wikimediaTopic = "wikimedia-recent-change";
 
-        RestHighLevelClient restHighLevelClient = OpenSearchConnection.create();
+        RestHighLevelClient openSearchClient = OpenSearchConnection.create();
+        KafkaConsumer<String, String> wikimediaKafkaConsumer = WikimediaKafkaConsumer.create();
 
-        try (restHighLevelClient) {
-            boolean isIndexExists = restHighLevelClient.indices().exists(new GetIndexRequest(wikimediaIndex), RequestOptions.DEFAULT);
+        try (openSearchClient; wikimediaKafkaConsumer) {
+            boolean isIndexExists = openSearchClient.indices().exists(new GetIndexRequest(wikimediaIndex), RequestOptions.DEFAULT);
 
+            wikimediaKafkaConsumer.subscribe(Collections.singleton(wikimediaTopic));
+
+            // Create index in OpenSearch
             if (!isIndexExists) {
-                restHighLevelClient.indices().create(new CreateIndexRequest(wikimediaIndex), RequestOptions.DEFAULT);
+                openSearchClient.indices().create(new CreateIndexRequest(wikimediaIndex), RequestOptions.DEFAULT);
                 logger.info("The wikimedia index has been created");
+            }
+
+            // Insert record into OpenSearch
+            while (true) {
+                ConsumerRecords<String, String> records = wikimediaKafkaConsumer.poll(Duration.ofMillis(3000));
+
+                for (ConsumerRecord<String, String> record : records
+                ) {
+                    IndexRequest indexRequest = new IndexRequest(wikimediaIndex).
+                            source(record.value(), XContentType.JSON);
+
+                    try {
+                        IndexResponse indexResponse = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        logger.info(indexResponse.getId());
+                    } catch (Exception e) {
+                        logger.error(e.getMessage());
+                    }
+                }
             }
         }
     }
